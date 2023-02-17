e7c2ec7a0f6a0b6fe413628aadb438fb37bd0855
kaulsalil88 <kaulsalil88@gmail.com>
2023-02-17 15:20:25 +0530

    Added the schema for the tasks

---
+++ src/DBScheme.js
@@ -0,0 +1,12 @@
+const TaskSchema = {
+    name: "Task",
+    properties: {
+      _id: "int",
+      name: "string",
+      status: "string?",
+      owner_id: "string?",
+    },
+    primaryKey: "_id",
+  };
+  
+export default TaskSchema  
